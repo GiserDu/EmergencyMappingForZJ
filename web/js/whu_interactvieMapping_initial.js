e0cd2ab8e87dcb81673ca2bd76d1e1bf145b1948
frankwym <wymzy111>
2018-10-29 00:14:32 +0800

    增加interactiveMapping.html

--- web/js/whu_interactvieMapping_initial.js
+++ web/js/whu_interactvieMapping_initial.js
@@ -12,8 +12,19 @@ var classGLayer,chartGLayer;//预设分级图层和统计图层
 var regionParam = 0;//regionParam为所选的区域代码(1:初始第一级17个地级市;其他:对应地州市的区域代码)
 var rgnName;//当前所选区域名称
 var geometry = new Object();//当前所选区域中心点对象
+var baseMap;//底图
+var layerNodesObj;
+var layerNodes =[
+    {id:1, pId:0, name:"地理底图", open:true, "nocheck":true},
+    {id:101, pId:1, name:"矢量图",url:"http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer",checked:true},
+    {id:102, pId:1, name:"影像图",url:"http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetGray/MapServer"},
+    {id:103, pId:1, name:"地形图",url:"http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineCommunityENG/MapServer"},
 
+    {id:2, pId:0, name:"专题服务图层",isParent:true, open:false, "nocheck":true},
 
+    {id:3, pId:0, name:"要素图层", isParent:true,open:false, "nocheck":true},
+
+];
 $(document).ready(function() {
     findDimensions();
     $("#mapContainer").height(winHeight);
@@ -46,22 +57,23 @@ $(document).ready(function() {
 
 
     function initMap() {
-        require(["esri/map"],function (Map) {
+        require(["esri/map","esri/layers/WebTiledLayer","esri/layers/ArcGISDynamicMapServiceLayer"],function (Map,WebTiledLayer,ArcGISDynamicMapServiceLayer) {
             map = new Map("mapContainer", {
-                basemap:"osm",
-                center: [113.2561395,31.01224452],
-                zoom: 7
-                //  center:   [120.78484503768885,28.933081922343742],
-
+               // basemap:"osm",
+                center: [104,35],
+                //zoom: 12
             });
+            baseMap = new ArcGISDynamicMapServiceLayer(
+                'http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer'
+            );
+            map.addLayer(baseMap)
             mapExtentChange = map.on("zoom-end", function zoomed() {
-                var zoomLevel = map.getZoom();
                 var zoomLevel = map.getZoom();
             });
         });
       }
 });
-
+//框选定位
 $("#RecNav").click(function () {
     require(["esri/toolbars/draw"],function (Draw) {
         tb= new Draw(map);
@@ -74,4 +86,119 @@ $("#RecNav").click(function () {
         tb.activate(Draw.EXTENT);    //激活相应的图形
     })
 });
+//制图
+$("#doMap").click(function () {
+    var setting = {
 
+        check: {
+            enable: true
+        },
+        data: {
+            simpleData: {
+                enable: true
+            }
+        },
+        edit: {
+            enable: true,
+            showRenameBtn: false,
+            showRemoveBtn: false
+        },
+        view:{
+            addHoverDom: addHoverDom,
+            removeHoverDom: removeHoverDom
+        },
+        callback: {
+            beforeCheck: layerOncheck
+        }
+    };
+    function addHoverDom(treeId, treeNode) {
+        var aObj = $("#" + treeNode.tId + "_a");
+
+        if(treeNode.isParent){
+            if ($("#doMapAdd_"+treeNode.id).length>0) return;
+            var editStr = "<span id='doMapAdd_"+treeNode.id+"' class='button doMapAdd'  onfocus='this.blur();'></span>";
+            aObj.append(editStr);
+
+
+            var btn = $("#doMapAdd_"+treeNode.id);
+            if (btn) btn.bind("click", function(){alert("添加" + treeNode.name);});
+
+        }
+        else{
+            if ($("#doMapEdit_"+treeNode.id).length>0) return;
+            var editStr = "<span id='doMapEdit_"+treeNode.id+"' class='button doMapEdit'  onfocus='this.blur();'></span>"+
+                "<span id='doMapRemove_"+treeNode.id+"' class='button doMapRemove'  onfocus='this.blur();'></span>";
+            aObj.append(editStr);
+
+
+            var btn = $("#doMapEdit_"+treeNode.id);
+            if (btn) btn.bind("click", function(){alert("编辑" + treeNode.name);});
+            var btn1 = $("#doMapRemove_"+treeNode.id);
+            if (btn1) btn1.bind("click", function(){alert("删除" + treeNode.name);});
+        }
+
+    };
+    function removeHoverDom(treeId, treeNode) {
+        if(treeNode.isParent){
+            $("#doMapAdd_" +treeNode.id).unbind().remove();
+        }
+        else{
+            $("#doMapEdit_" +treeNode.id).unbind().remove();
+            $("#doMapRemove_" +treeNode.id).unbind().remove();
+        }
+
+    };
+
+
+    layerNodesObj=$.fn.zTree.init($("#doMapTree"), setting, layerNodes);
+    layui.use('layer', function (layui_index) {
+        var layer = layui.layer;
+        layer.open({
+            title: '专题图层',
+            skin: "layui-layer-lan",
+            type: 1,
+            shade: 0,
+            content:$('#doMapTree'),
+            yes: function(index, layero) {//确定后执行回调
+
+            }});
+
+
+    });
+
+})
+
+//图层check事件
+function layerOncheck(treeId, treeNode) {
+    if(treeNode.isParent){
+        return;
+    }
+    else {
+        if(treeNode.pId==1){
+            //地理底图
+            require(["esri/layers/WebTiledLayer","esri/layers/ArcGISDynamicMapServiceLayer"],function (WebTiledLayer,ArcGISDynamicMapServiceLayer) {
+                var mate=treeNode.getParentNode().children;
+                for (var  i = 0; i < mate.length; i++) {
+                    if(mate[i].id==treeNode.id)
+                        continue;
+                    var node = layerNodesObj.getNodeByTId(mate[i].tId);
+                    layerNodesObj.checkNode(node, false, true);
+
+                }
+
+
+                map.removeLayer(baseMap);
+                baseMap = new ArcGISDynamicMapServiceLayer(
+                    treeNode.url
+                );
+                map.addLayer(baseMap)
+
+                map.reorderLayer(baseMap,1);
+            });
+
+
+
+
+        }
+    }
+}
