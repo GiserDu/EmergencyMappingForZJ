0bc756b365fdf8acce15fb16af68ba8d5cac1f9d
frankwym <wymzy111>
2018-10-29 17:40:43 +0800

    test

--- web/js/whu_interactvieMapping_initial.js
+++ web/js/whu_interactvieMapping_initial.js
@@ -154,7 +154,7 @@ $("#doMap").click(function () {
     layui.use('layer', function (layui_index) {
         var layer = layui.layer;
         layer.open({
-            title: '交互壶色弱1',
+            title: '交互制图',
             skin: "layui-layer-lan",
             type: 1,
             shade: 0,
