773313a58269c4820b24f72cc3756328458d38a7
frankwym <wymzy111>
2018-11-17 14:26:27 +0800

    优化行政区定位

--- src/telecarto/geoinfo/servlets/GetAdministrativeRegion.java
+++ src/telecarto/geoinfo/servlets/GetAdministrativeRegion.java
@@ -1,6 +1,7 @@
 package telecarto.geoinfo.servlets;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import telecarto.geoinfo.db.MysqlAccessBean;
 
@@ -9,20 +10,63 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @WebServlet(name = "GetAdministrativeRegion")
 public class GetAdministrativeRegion extends HttpServlet {
+
+//从服务接口获取json，主要为了从地理空间数据云中爬取数据
+        public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+            InputStream is = new URL(url).openStream();
+            try {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                StringBuilder sb = new StringBuilder();
+                int cp;
+                while ((cp = rd.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                String jsonText = sb.toString();
+                JSONObject json = JSONObject.fromObject(jsonText);
+                return json;
+            } finally {
+                is.close();
+            }
+        }
+
+    /**
+     * JSON字符串特殊字符处理，比如：“\A1;1300”
+     * @param s
+     * @return String
+     */
+    public static String stringToJson(String s,boolean isJSjson){
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<s.length(); i++){
+            char c =s.charAt(i);
+            switch(c){
+                case'\'': if(isJSjson) {sb.append("\\\'");}else{sb.append("\'");} break;
+                case'\"': if(!isJSjson) {sb.append("\\\"");}else{sb.append("\"");} break;
+                case'\\':sb.append("\\\\"); break; //如果不处理单引号，可以释放此段代码，若结合StringDanYinToJSON()处理单引号就必须注释掉该段代码
+                case'/': sb.append("\\/");break;
+                case'\b':sb.append("\\b");break;//退格
+                case'\f':sb.append("\\f");break;//走纸换页
+                case'\n':sb.append("\\n");break;//换行
+                case'\r':sb.append("\\r");break;//回车
+                case'\t':sb.append("\\t");break;//横向跳格
+                default: sb.append(c);
+            }}
+        return sb.toString();
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         String type=request.getParameter("type");
         String regionName="";
         String regionCode="";
-        System.out.println("11111");
+        System.out.println("*91311111");
         MysqlAccessBean mysql = null;
         ResultSet resultSet = null;
         String proCode="";
@@ -37,30 +81,32 @@ public class GetAdministrativeRegion extends HttpServlet {
                     String sql = "";
                     JSONArray jsonarray = new JSONArray();
                     for(int i =0;i<selectedRegion.length;i++){
+
                         if(code.equals("proCode")){
-                            sql = "SELECT gson from regionboundary1 WHERE  citycode = '' and coutcode ='' and "+code+"='";
+                            sql = "SELECT gson from regionboundarys_copy WHERE  citycode = '' and coutcode ='' and name='";
                         }
                         else if(code.equals("cityCode")){
-                            sql = "SELECT gson from regionboundary1 WHERE  coutcode ='' and citycode !='' and "+code+"='";
+                            sql = "SELECT gson from regionboundarys_copy WHERE  coutcode ='' and citycode !='' and name='";
                         }
                         else if(code.equals("coutCode")){
-                            sql = "SELECT gson from regionboundary1 WHERE coutcode !='' and "+code+"='";
+                            sql = "SELECT gson from regionboundarys_copy WHERE coutcode !='' and name='";
                         }
                         sql=sql+selectedRegion[i]+"'";
+
+                       /* if(code.equals("proCode")){
+                            sql = "SELECT coutcode from regionboundarys_copy WHERE coutcode !=''";
+                        }*/
+
                         resultSet = mysql.query(sql);
                         while (resultSet.next()) {
+                            /*String currentCode=resultSet.getString(1);
+                            JSONObject jsons= readJsonFromUrl("http://www.gscloud.cn/statenames/coutjson/"+currentCode);
+                            String updateSQL="update regionboundarys_copy set gson='"+jsons+"' where  coutcode ='"+currentCode+"'";
+                            mysql.update(updateSQL);*/
                             regionName=resultSet.getString(1);
-                            /*switch (x = x.replaceAll("\\\\", "@@@@@")) {
-                            }*/
-
-                            /*JSONObject json=JSONObject.fromObject(x);
-                            jsonarray.add(json);*/
-                            //regionName=regionName+resultSet.getString(1)+"s-p-ll";
-
+                            regionName=stringToJson(regionName,true);
                         }
                     }
-
-                    // regionName=regionName.substring(0,regionName.length()-6).trim();
                     PrintWriter out = response.getWriter();
 
                     out.println(regionName);
